4a7edecb8ca99f30e0cd78ad1e81fb8b71bf539a
Steven Choi <stevencschoi87@gmail.com>
2020-04-10 03:13:13 +0000

    added image sources for db

--- db/seeds/07_ingredients.sql
+++ db/seeds/07_ingredients.sql
@@ -1,12 +1,12 @@
 INSERT INTO ingredients (name, calories, fat_in_g, carbs_in_g, sugar_in_g, sodium_in_mg, cholesterol_in_mg, protein_in_g, fiber_in_g, iron_in_mg, calcium_in_mg, image_url) VALUES (
-  'spinach', 7, 0, 1, 0, 25, 0, 1, 0.7, 0.9, 31, 'imgurl'
+  'spinach', 7, 0, 1, 0, 25, 0, 1, 0.7, 0.9, 31, '../../public/assets/spinach.jpg'
 ),
 (
-  'mushrooms', 12, 0, 2, 1, 2, 0, 2, 0.6, 0.3, 2, 'imgurl'
+  'mushrooms', 12, 0, 2, 1, 2, 0, 2, 0.6, 0.3, 2, '../../public/assets/mushrooms.jpg'
 ),
 (
-  'carrots', 25, 0, 6, 3, 42, 0, 1, 1.5, 0.2, 20, 'imgurl'
+  'carrots', 25, 0, 6, 3, 42, 0, 1, 1.5, 0.2, 20, '../../public/assets/carrots.jpg'
 ),
 (
-  'eggs', 78, 5, 1, 0, 62, 216, 6, 0, 0.6, 25, 'imgurl'
+  'eggs', 78, 5, 1, 0, 62, 216, 6, 0, 0.6, 25, '../../public/assets/eggs.jpg'
 );
