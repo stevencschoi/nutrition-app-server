78813ca503a13c11cfdfd4af5cd8ca8d41f20302
stevencschoi <stevencschoi87@gmail.com>
2020-04-13 15:25:03 -0400

    connected db to server

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 8008;
+const ENV = process.env.ENV || "development";
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 const server = require("http").Server(app);
 // const cookieSession = require('cookie-session');
 
-const PORT = process.env.PORT || 8008;
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+
+app.use(morgan("dev"));
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("../lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+const databaseHelperFunctions = require("../routes/database")(db);
 
 // on the request to root (localhost:8008)
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
+  res.send("Hello, world!");
+  databaseHelperFunctions.test();
+  console.log(dbParams);
+});
+
+app.put("/register", function (req, res) {});
+
+app.post("/favourites/:id", function (req, res) {
   res.send("Hello, world!");
 });
 
 // Change the 404 message modifing the middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).send("Page not found!)");
 });
 
