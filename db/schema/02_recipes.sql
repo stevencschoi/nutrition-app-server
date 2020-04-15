4c44c0fe332f1023626c18293bca930770e6f030
stevencschoi <stevencschoi87@gmail.com>
2020-04-15 11:22:41 -0400

    addded seeds for db

--- db/schema/02_recipes.sql
+++ db/schema/02_recipes.sql
@@ -2,18 +2,14 @@ DROP TABLE IF EXISTS recipes CASCADE;
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  image_url TEXT,
-  ingredients TEXT[] NOT NULL,
-  prep_time_in_minutes INTEGER,
+  name VARCHAR(255) NOT NULL,
   calories INTEGER,
-  saturated_fat_in_g INTEGER,
+  fat_in_g INTEGER,
   carbs_in_g INTEGER,
-  sugar_in_g INTEGER,
-  sodium_in_mg INTEGER,
-  cholesterol_in_mg INTEGER,
   protein_in_g INTEGER,
+  sugar_in_g INTEGER,
   fiber_in_g INTEGER,
-  iron_in_mg INTEGER,
-  vitamin_c_in_mg INTEGER,
-  calcium_in_mg INTEGER
+  cholesterol_in_mg INTEGER,
+  sodium_in_mg INTEGER,
+  image_url TEXT
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,23 +1,23 @@
 INSERT INTO users (username, name, email, password, avatar) VALUES (
-  'Steven', 'Steven', 'steven@vegtable.com', 'password', 
+  'steven', 'Steven', 'steven@vegtable.com', 'password', 
   '/assets/steven-choi-2.JPG'
 ),
 (
-  'Marco', 'Marco', 'marco@vegtable.com', 'password', '/assets/marco.jpeg'
+  'marco', 'Marco', 'marco@vegtable.com', 'password', '/assets/marco.jpeg'
 ),
 (
-  'Victor', 'Victor', 'victor@vegtable.com', 'password', 
+  'victor', 'Victor', 'victor@vegtable.com', 'password', 
   '/assets/victor.jpeg'
 ),
 (
-  'Daffy', 'Daffy Duck', 'daffy@looneytunes.com', 'password', '/assets/daffy.jepg'
+  'daffy', 'Daffy Duck', 'daffy@looneytunes.com', 'password', '/assets/daffy.jepg'
 ),
 (
-  'Eric', 'Eric Cartman', 'eric@southpark.com', 'password', '/assets/eric.jpeg'
+  'eric', 'Eric Cartman', 'eric@southpark.com', 'password', '/assets/eric.jpeg'
 ),
 (
-  'Garfield', 'Garfield', 'garfield@garfield.com', 'password', '/assets/garfield.jpeg'
+  'garfield', 'Garfield', 'garfield@garfield.com', 'password', '/assets/garfield.jpeg'
 ),
 (
-  'Homer', 'Homer Simpson', 'homer@simpson.com', 'password', '/assets/homer.jpg'
+  'homer', 'Homer Simpson', 'homer@simpson.com', 'password', '/assets/homer.jpg'
 )
