e4384f6ba584b14c09eb55234faca1fa9d1288ce
Victor <victorcwyu@gmail.com>
2020-05-01 22:08:49 -0400

    Fixed seeds, so names are capitalized.

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,23 +1,23 @@
 INSERT INTO users (username, name, email, password, avatar) VALUES (
-  'steven', 'Steven', 'steven@vegtable.com', 'password', 
+  'Steven', 'Steven', 'steven@vegtable.com', 'password', 
   'https://avatars1.githubusercontent.com/u/50030216?s=460&u=5dfad17993c8a3545bb99b21d6fdf26778d4cf9c&v=4'
 ),
 (
-  'marco', 'Marco', 'marco@vegtable.com', 'password', 'https://avatars0.githubusercontent.com/u/50462559?s=460&u=936f68c8e134088a7404c2aeec202c956ad76b0e&v=4'
+  'Marco', 'Marco', 'marco@vegtable.com', 'password', 'https://avatars0.githubusercontent.com/u/50462559?s=460&u=936f68c8e134088a7404c2aeec202c956ad76b0e&v=4'
 ),
 (
   'victor', 'Victor', 'victor@vegtable.com', 'password', 
   'https://avatars3.githubusercontent.com/u/36527985?s=460&u=5c16998aefef3cb20e105c173e8c92ad5e6e40c8&v=4'
 ),
 (
-  'daffy', 'Daffy Duck', 'daffy@looneytunes.com', 'password', 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F_ohmOoHK96yM%2FTSdssJ6fyFI%2FAAAAAAAAAd0%2FvtGyVfdocRY%2Fs1600%2FDaffy_010.jpg&f=1&nofb=1'
+  'Daffy', 'Daffy Duck', 'daffy@looneytunes.com', 'password', 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F_ohmOoHK96yM%2FTSdssJ6fyFI%2FAAAAAAAAAd0%2FvtGyVfdocRY%2Fs1600%2FDaffy_010.jpg&f=1&nofb=1'
 ),
 (
-  'eric', 'Eric Cartman', 'eric@southpark.com', 'password', 'https://vignette.wikia.nocookie.net/southpark/images/c/c4/Eric-cartman.png/revision/latest?cb=20171121092247'
+  'Eric', 'Eric Cartman', 'eric@southpark.com', 'password', 'https://vignette.wikia.nocookie.net/southpark/images/c/c4/Eric-cartman.png/revision/latest?cb=20171121092247'
 ),
 (
-  'garfield', 'Garfield', 'garfield@garfield.com', 'password', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.thefrugalgene.com%2Fwp-content%2Fuploads%2F2017%2F11%2Fgarfield-clipart-sad-2-min.jpg%3Ffit%3D736%252C827%26ssl%3D1&f=1&nofb=1'
+  'Garfield', 'Garfield', 'garfield@garfield.com', 'password', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.thefrugalgene.com%2Fwp-content%2Fuploads%2F2017%2F11%2Fgarfield-clipart-sad-2-min.jpg%3Ffit%3D736%252C827%26ssl%3D1&f=1&nofb=1'
 ),
 (
-  'homer', 'Homer Simpson', 'homer@simpson.com', 'password', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.comicvine.com%2Fuploads%2Foriginal%2F11123%2F111232312%2F5638218-homer-simpson-2543.jpg&f=1&nofb=1'
+  'Homer', 'Homer Simpson', 'homer@simpson.com', 'password', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.comicvine.com%2Fuploads%2Foriginal%2F11123%2F111232312%2F5638218-homer-simpson-2543.jpg&f=1&nofb=1'
 )
